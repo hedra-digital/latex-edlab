e71d4ff478557394da3c03c709b66e8f42b6e6be
Jorge Sallum <jorge@hedra.com.br>
2022-09-21 17:08:29 -0300

    aproveitando coisas do PNLD

---
+++ edlab-logos/run.sh
@@ -0,0 +1,11 @@
+mv _7letras.png 7letras.png
+mv _aarea.png aarea.png
+mv _bikini.jpeg bikini.jpeg
+mv _bikini.png bikini.png
+mv _camilawerner.png camilawerner.png
+mv _cobogo.png cobogo.png
+mv _educacaofinanceira.png educacaofinanceira.png
+mv _logopoiese.png logopoiese.png
+mv _patua.png patua.png
+mv _saira.png saira.png
+mv _ubu.png ubu.png
